df114ee9197d9599af60926ff8bd794fbb28a06e
sterben04 <aravindunni00@gmail.com>
2021-07-05 11:19:33 +0530

    database created

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'aasasasaasd'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
 
 
     from .views import views
@@ -10,6 +18,15 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-    
+
+    from .models import User,Note #importing class from models.py
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/'+ DB_NAME):
+        db.create_all(app=app)
+        print('Created Database')
 
--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,45 @@
-from flask import Blueprint
-from flask.templating import render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db #from init.py
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    data = request.form
+    print(data)
+    return render_template("login.html",boolean=True)
+
 
 @auth.route('/logout')
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 char', category="error")
+        elif len(first_name) <= 2:
+            flash('First Name must be greater than 2 char', category="error")
+        elif len(password1) < 7:
+            flash('Password must be greater than 6 char', category="error")
+        elif password1 != password2:
+            flash('Passwords don\'t match', category="error")
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash('Account Created', category='success')
+            return redirect(url_for('views.home'))   #fn home in views.py
+
     return render_template("sign_up.html")
